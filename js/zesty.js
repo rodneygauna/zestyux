0dbcd28f51b960cf0dbd367b6352db27379bdfd0
Rodney Gauna <rodneygauna@gmail.com>
2023-08-23 23:55:08 +0000

    getting dark mode implemented

--- js/zesty.js
+++ js/zesty.js
@@ -15,7 +15,7 @@
 
 /* Side Menu Functionality
 --------------------------- */
-const sideMenu = document.querySelector('aside');
+/* const sideMenu = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
 
@@ -25,16 +25,30 @@ menuBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
-});
+}); */
 
 /* Dark Mode Functionality
 --------------------------- */
 const darkMode = document.querySelector('.dark-mode');
+const userHasDarkMode = localStorage.getItem('dark-mode') === 'enabled';
+
+if (userHasDarkMode) {
+    document.body.classList.add('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.remove('active');
+    darkMode.querySelector('span:nth-child(2)').classList.add('active');
+}
 
 darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+    // Remember user preference in local storage
+    if (document.body.classList.contains('dark-mode-variables')) {
+        localStorage.setItem('dark-mode', 'enabled');
+    }
+    else {
+        localStorage.setItem('dark-mode', 'disabled');
+    }
 })
 
 /* Banner Functionality
