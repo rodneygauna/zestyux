08cd4737ffdc6c18bc985d23572b4b2afceeba07
Rodney Gauna <rodneygauna@gmail.com>
2023-08-23 16:51:28 +0000

    early prep for menu toggles and theme switch

---
+++ js/zesty.js
@@ -0,0 +1,27 @@
+/*
+* Zesty CSS
+* Copyright 2023, Rodney Gauna
+* www.zestyux.com
+* Free to use under the GNU GPL v3.0 license
+* http://www.gnu.org/licenses/gpl.html
+*/
+
+const sideMenu = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sideMenu.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
