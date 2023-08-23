a85c1444dc2a1dda80c63fb20be692974d5d8adf
Rodney Gauna <rodneygauna@gmail.com>
2023-08-23 23:07:05 +0000

    code, cards, and banners now added

--- js/zesty.js
+++ js/zesty.js
@@ -25,3 +25,35 @@ darkMode.addEventListener('click', () => {
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
 })
+
+/* Banner Functionality */
+var fadeOutInterval;
+
+function showBanner() {
+    var banner = document.getElementsByClassName("banner")[0];
+    banner.style.display = "flex";
+    resetHideTimer();
+}
+
+function hideBanner() {
+    var banner = document.getElementsByClassName("banner")[0];
+    banner.style.opacity = 1;
+    fadeOutInterval = setInterval(function() {
+        if (banner.style.opacity > 0) {
+            banner.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeOutInterval);
+            banner.style.display = "none";
+        }
+    }, 60);
+}
+
+function resetHideTimer() {
+    clearInterval(fadeOutInterval);
+    fadeOutInterval = setTimeout(hideBanner, 5000);
+}
+
+var bannerSuccess = document.getElementsByClassName("banner success")[0];
+if (bannerSuccess) {
+    resetHideTimer();
+}
